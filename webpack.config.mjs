3ca765c42ccce1de2f89668bccb8fe9dd6b54304
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-05-22 22:28:35 -0300

    feat: Module Federation

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -231,6 +231,22 @@ export default (env) => {
           assetsPath,
         },
       }),
+      new Repack.plugins.ModuleFederationPlugin({
+        name: 'repackloan',
+        exposes: {
+          './App': './App.tsx',
+        },
+        shared: {
+          react: {
+            ...Repack.Federated.SHARED_REACT,
+            requiredVersion: '18.2.0',
+          },
+          'react-native': {
+            ...Repack.Federated.SHARED_REACT_NATIVE,
+            requiredVersion: '0.71.7',
+          },
+        },
+      }),            
     ],
   };
 };
